87385e3beb3d4968edf51ceff78530d5476387cf
ShuheiOhno <inu.suika8@gmail.com>
2021-11-28 12:12:42 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,12 +1,55 @@
-# ../が一つ増えている
-app_path = File.expand_path('../../../', __FILE__)
+# rootパスのディレクトリを指定
+root_path = File.expand_path('../../', __FILE__)
 
-worker_processes 1
-# currentを指定
-working_directory "#{app_path}/current"
+# アプリケーションサーバの性能を決定する
+worker_processes 2
 
-# それぞれ、sharedの中を参照するよう変更
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+# アプリケーションの設置されているディレクトリを指定
+working_directory root_path
+
+# プロセスIDの保存先を指定
+pid "#{root_path}/tmp/pids/unicorn.pid"
+
+# ポート番号を指定
+listen "#{root_path}/tmp/sockets/unicorn.sock"
+
+# エラーのログを記録するファイルを指定
+stderr_path "#{root_path}/log/unicorn.stderr.log"
+
+# 通常のログを記録するファイルを指定
+stdout_path "#{root_path}/log/unicorn.stdout.log"
+
+#応答時間を待つ上限時間を設定
+timeout 30
+
+# ダウンタイムなしでUnicornを再起動時する
+preload_app true
+
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+run_once = true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+
+  if run_once
+    run_once = false # prevent from firing again
+  end
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exist?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH => e
+      logger.error e
+    end
+  end
+end
+
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+end
